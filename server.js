6a0889f5d2bfc78dcf6ddc6dd65d6d64705bb4b7
Mronz <kamil.2003.kmz@gmail.com>
2022-02-01 23:52:16 +0100

    finito

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
-var express = require("express")
-var app = express()
-const PORT = 3000;
+var express = require("express");
+var app = express();
+const PORT = process.env.PORT || 3000;
 var hbs = require('express-handlebars');
-var path = require("path")
+var path = require("path");
+app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs', hbs({ defaultLayout: 'index.hbs' }));
+app.set('view engine', 'hbs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 var login = false
 var users = [
-    { login: 'admin', password: 'admin', age: '99', student: 'on', plec: 'M' }
+    { id: 1, login: "AAA", password: "PASS1", age: '10', student: "checked", plec: "M" },
+    { id: 2, login: "BBB", password: "PASS2", age: '9', student: "checked", plec: "K" },
+    { id: 3, login: 'admin', password: 'admin', age: '99', student: 'checked', plec: 'M' }
 ]
-var context = {
-    create: "użytkownik stworzony",
-    login: "zalogowany",
-    users: users
-}
-
-app.use(express.json());
-
-app.use(express.urlencoded({
-    extended: true
-}));
-
-app.set('views', path.join(__dirname, 'views'));         // ustalamy katalog views
-app.engine('hbs', hbs({ defaultLayout: 'index.hbs' }));   // domyślny layout, potem można go zmienić
-app.set('view engine', 'hbs');
+var id = 2;
 
 app.get("/", function (req, res) {
-    res.render('main.hbs');
+    context = {};
+    if (login == true) {
+        context.logout = "logout";
+    }
+    res.render('main.hbs', context);
 })
 app.get("/register", function (req, res) {
+    context = {};
+    if (login == true) {
+        context.logout = "logout";
+    }
     res.render('register.hbs');
 })
 app.post("/handlePost", function (req, res) {
@@ -36,68 +37,108 @@ app.post("/handlePost", function (req, res) {
         if (req.body.login == users[i].login) {
             temp++;
         }
-
     }
     if (temp == 0) {
-        users.push(req.body)
-        context.create = "użytkownik stworzony"
-        res.render('register.hbs', context);
+        id++;
+        req.body.id = id;
+        if (req.body.plec == undefined) req.body.plec = "undefined";
+        users.push(req.body);
+        res.redirect('/login');
     }
     else {
-        context.create = "użytkownik już istnieje"
-        res.render('register.hbs', context)
+        res.redirect('/register');
     }
 })
 
 app.get("/login", function (req, res) {
-    res.render('login.hbs');
+    context = {};
+    if (login == true) {
+        context.logout = "logout";
+    }
+    res.render('login.hbs', context);
 })
 
 app.post("/handlePostL", function (req, res) {
     var temp = -1;
-    var status = false
     for (let i = 0; i < users.length; i++) {
         if (req.body.login == users[i].login) {
             temp = i;
             if (req.body.password == users[temp].password) {
-                context.login = "zalogowano"
-                res.render('login.hbs', context);
-                status = true;
+                res.redirect('/');
                 login = true;
             }
         }
     }
-    if (status == false) {
-        context.login = "złe dane"
-        res.render('login.hbs', context);
+    if (login == false) {
+        res.redirect('/login');
     }
-    console.log(users);
-
 })
 
 app.get("/admin", function (req, res) {
-    if (login == true) res.render('adminL.hbs')
-    else res.render('admin.hbs');
+    context = {};
+    if (login == true) {
+        context.logout = "logout";
+        res.render('adminL.hbs', context);
+    }
+    else {
+        context = {};
+        res.render('admin.hbs', context);
+    }
+})
+
+app.get("/logout", function (req, res) {
+    login = false;
+    res.redirect("/");
 })
 
 app.get("/sort", function (req, res) {
-    var sorted = `<table>`
-    for (let i = 0; i < users.length; i++) {
-        sorted += `<tr>
-        <td>`+ users[i].login + `</td>
-        <td>`+ users[i].password + `</td>
-        <td>`+ users[i].age + `</td>
-        <td>`+ users[i].student + `</td>
-        <td>`+ users[i].plec + `</td>
-        </tr>`
+    context = {};
+    if (login == true) {
+        context.logout = "logout";
     }
-    sorted += `</table>`
-    context.users = sorted;
-    console.log(sorted)
-    res.setHeader("content-type", "application/json")
+
+
+    if (req.query.sort == "up") {
+        users.sort(function (a, b) {
+            return parseFloat(a.wiek) - parseFloat(b.wiek);
+        });
+    }
+    else {
+        users.sort(function (a, b) {
+            return parseFloat(a.wiek) - parseFloat(b.wiek);
+        }).reverse();
+    }
+    context.data = users;
+
     res.render('sort.hbs', context);
 })
 
+app.get("/gender", function (req, res) {
+    context = {};
+    if (login == true) context.logout = "logout";
+    k = [];
+    o = [];
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].plec == "K") k.push(users[i]);
+        else o.push(users[i]);
+    }
+    context.k = k;
+    context.i = o;
+    res.render('gender.hbs', context);
+})
+
+app.get("/show", function (req, res) {
+    context = {};
+    if (login == true) context.logout = "logout";
+
+    users.sort(function (a, b) {
+        return parseFloat(a.wiek) - parseFloat(b.wiek);
+    }).reverse();
+    context.data = users;
+
+    res.render('show.hbs', context);
+})
 
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
